de903623bbc0212105cb068b77fac1f2bef4fa29
Neha Bhomia <nehabhomia@hotmail.co.uk>
2019-03-24 14:13:08 -0400

    split the dataset into test and train

--- task1_classifier.py
+++ task1_classifier.py
@@ -10,8 +10,10 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, train_test_split
 
 #importing data
 
 tweets = pd.read_csv('task1_training.tsv', delimiter='\t')
+
+tweets_train, tweets_test = train_test_split(tweets, test_size=0.20, random_state=42)
