cdbb1aeb3e11fb1d8a053dd0c2cd71ee3342d402
Bryan <bcromas@gmail.com>
2019-03-26 17:34:23 -0400

    added recursive call to get related tweets

--- get_tweets_tweepy.py
+++ get_tweets_tweepy.py
@@ -27,23 +27,34 @@ api = tweepy.API(auth)
 #End OAuth code
 
 FILENAME = 'test_tweet_ids.txt' #this is just a sub-set of tweet ids taken from 'training_set_1_ids.txt'
+# FILENAME = 'test_tweet_ids_10.txt'
 
 #Start cache check
 def get_tweet(found_id):
     if found_id in CACHE_DICTION:
+        #if we get strange results in cache like missing child tweets then we may need to add the call to get_tweet() here
         return CACHE_DICTION[found_id]
     else:
         try:
             resp = api.get_status(found_id) #resp is a class 'tweepy.models.Status'
             json_str = json.dumps(resp._json) #json_str var is str type
             json_obj = json.loads(json_str) #json_obj var is dict type
-        except:
+        except Exception as e:
             json_obj = {}
+            message = 'No worries. Empty entry has been made in cache.'
+            print('Exception in get_tweet:{}\nProblematic Tweet:{}\n\n{}\n'.format(e, found_id,message))
         CACHE_DICTION[found_id] = json_obj #creating new entry in cache dict where key = 'found_id' & value = 'json_obj' which is a dict
         dumped_json_cache = json.dumps(CACHE_DICTION)
         fw = open(CACHE_FNAME,"w")
         fw.write(dumped_json_cache)
         fw.close() # close the open file
+
+        #start recursive call to get_tweet() a.k.a. this funct if current tweet is in reply to another tweet
+        if CACHE_DICTION[found_id]['in_reply_to_status_id_str'] is not None:
+            in_reply_id = CACHE_DICTION[found_id]['in_reply_to_status_id_str']
+            get_tweet(in_reply_id) 
+        #end recursive call to get_tweet()
+
         return CACHE_DICTION[found_id]
 #End cache check
 
@@ -53,8 +64,8 @@ def read_in_tweet_ids():
             found_id = line.split('\t')[0]
             try:
                 get_tweet(found_id)
-            except Exception as e:
-                print(e)
+            except:
+                pass #having exception message print here was causing duplicate messages when get_tweet failed
 
 if __name__ == "__main__":
     check = read_in_tweet_ids()
